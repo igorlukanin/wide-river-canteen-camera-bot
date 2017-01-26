0c06e2a97e2b9f0a5fa314d2f8e332ced944f587
Igor Lukanin <mail@igor.lukanin.name>
2017-01-26 14:40:17 +0500

    Retrieving real snapshots from camera

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,6 @@
 const config = require('config');
+const fs = require('fs');
+const request = require('request');
 const TelegramBot = require('node-telegram-bot-api');
 
 const db = require('./db');
@@ -6,7 +8,21 @@ const db = require('./db');
 
 const token = config.get('telegram.token');
 const button = config.get('bot.button');
+const username = config.get('camera.username');
+const password = config.get('camera.password');
+const snapshotUri = config.get('camera.snapshotUri');
+const cacheIntervalMs = config.get('camera.cacheIntervalMs');
+const cacheTemporaryPath = './snapshot-temporary.jpeg';
+const cachePath = './snapshot.jpeg';
 
+
+const updateSnapshot = () => request(snapshotUri)
+    .auth(username, password)
+    .pipe(fs.createWriteStream(cacheTemporaryPath))
+    .on('close', () => {
+        fs.createReadStream(cacheTemporaryPath)
+            .pipe(fs.createWriteStream(cachePath));
+    });
 
 const createReplyMarkup = button => JSON.stringify({
     keyboard: [ [ button ] ],
@@ -14,11 +30,20 @@ const createReplyMarkup = button => JSON.stringify({
 });
 
 
+// Schedule snapshot updates
+
+updateSnapshot();
+
+setInterval(updateSnapshot, cacheIntervalMs);
+
+
+// Start the bot
+
 const bot = new TelegramBot(token, { polling: true });
 const replyMarkupKeyboard = createReplyMarkup(button);
 
 bot.onText(/.*/, message => {
-    bot.sendPhoto(message.chat.id, 'placeholder.png', {
+    bot.sendPhoto(message.chat.id, cachePath, {
         reply_markup: replyMarkupKeyboard
     });
 
